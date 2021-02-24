c5394c75fdcb76e600a96dfa9ec3810fbec27934
funny986 <ddomnin@gmail.com>
2021-02-24 16:44:21 +0300

    add database

---
+++ app/src/main/java/com/dreambook/BothSideCoordinatorLayout.java
@@ -0,0 +1,61 @@
+package com.dreambook;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.ImageButton;
+import com.xenione.libs.swipemaker.AbsCoordinatorLayout;
+import com.xenione.libs.swipemaker.SwipeLayout;
+
+public class BothSideCoordinatorLayout extends AbsCoordinatorLayout {
+
+    private ImageButton mDelete;
+    private ImageButton mAction;
+    public SwipeLayout mForegroundView;
+
+    public BothSideCoordinatorLayout(Context context) {
+        super(context);
+    }
+
+    public BothSideCoordinatorLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public BothSideCoordinatorLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public BothSideCoordinatorLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void doInitialViewsLocation() {
+        mForegroundView = findViewById(R.id.foregroundView);
+        mDelete = findViewById(R.id.delete);
+        mAction = findViewById(R.id.action);
+//        mForegroundView.anchor(-mAction.getWidth(), 0, mDelete.getRight());
+        mForegroundView.anchor(-mDelete.getRight(), 0, mAction.getLeft());
+
+    }
+
+    @Override
+    public void onTranslateChange(float global, int index, float relative) {
+        if (index == 0) {
+            if (relative == 0) {
+
+            } else {
+            }
+        } else {
+            if (index == 1 && relative > 0) {
+            }
+            if (index == 1 && relative == 1){
+            }
+        }
+    }
+
+
+}
